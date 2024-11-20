6ddec320cd1fdfb681414ba4d0182724ba7e05b7
Omotola <omotolaodumosu04@gmail.com>
2024-11-20 11:48:30 +0100

    TS and JS consoleOveride

---
+++ JS consoleOveride/consoleOverride.js
@@ -0,0 +1,8 @@
+const originalLog = console.log;
+console.log = function (data) {
+    if (typeof data ==="object"){
+        data = JSON.stringify(data, null, 2);
+    }
+    // call the original console.log with the formatted data
+    originalLog(data);
+};
